b11b0ccb7cb590b49333d244ab4297f8a96ee9ab
David Ricketts <david.ricketts119@gmail.com>
2022-09-02 04:53:44 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//first commit
 export default function App() {
   return (
     <View style={styles.container}>
